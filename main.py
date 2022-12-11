2c9cee53693ecb694a42c3f46a5f538997ce530f
Shivam Mehrotra <shivammehrotra09@yahoo.in>
2022-12-11 19:04:31 +0530

    Added Table

--- main.py
+++ main.py
@@ -6,12 +6,38 @@ from pathlib import Path
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
-    filename = Path(filepath).stem
-    invoice_no = filename.split("-")[0]
-    pdf.set_font(family="Times", style="B", size=16)
-    pdf.cell(w=50, h=8, txt=f"Invoice no.{invoice_no}")
-    pdf.output(f"PDFs/{filename}.pdf")
 
+    filename = Path(filepath).stem
+    invoice_no, invoice_date = filename.split("-")
+
+    pdf.set_font(family="Times", style="B", size=16)
+    pdf.cell(w=50, h=8, txt=f"Invoice no.{invoice_no}", ln=1)
+
+    pdf.set_font(family="Times", style="B", size=16)
+    pdf.cell(w=50, h=8, txt=f"Invoice no.{invoice_date}", ln=1)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    # Add a header from Excel
+    header = df.columns
+    header = [item.replace("_", " ").title() for item in header]
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=header[0], border=1, align="C")
+    pdf.cell(w=60, h=8, txt=header[1], border=1, align="C")
+    pdf.cell(w=40, h=8, txt=header[2], border=1, align="C")
+    pdf.cell(w=30, h=8, txt=header[3], border=1, align="C")
+    pdf.cell(w=30, h=8, txt=header[4], border=1, align="C", ln=1)
+
+    # Add rows from Excel
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1, align="C")
+        pdf.cell(w=60, h=8, txt=str(row["product_name"]), border=1, align="C")
+        pdf.cell(w=40, h=8, txt=str(row["amount_purchased"]), border=1, align="C")
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1, align="C")
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, align="C", ln=1)
+
+    pdf.output(f"PDFs/{filename}.pdf")
